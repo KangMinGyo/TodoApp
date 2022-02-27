8354372f442b25d15f5836b99665422fc47594be
KangMinGyo <kang15567@kakao.com>
2022-02-27 15:49:49 +0900

    feat: TableView에 할 일 목록 추가

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController, UITableViewDataSource  {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var todo: [String] = ["iOS개발하기", "알고리즘 풀기"]
+    var todo: [String] = ["iOS 개발하기", "알고리즘 풀기"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todo.count
@@ -19,7 +19,7 @@ class ViewController: UIViewController, UITableViewDataSource  {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: TodoTableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TodoTableViewCell
-        
+        cell.todoLabel.text = todo[indexPath.row]
         return cell
     }
     
@@ -27,6 +27,7 @@ class ViewController: UIViewController, UITableViewDataSource  {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "할 일"
     }
 
 
