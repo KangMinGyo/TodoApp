a2e2834d1b540dc67873b5319b3773616c422e50
KangMinGyo <kang15567@kakao.com>
2022-02-27 21:28:20 +0900

    feat: Dismiss추가

---
+++ Todo/AddTaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddTaskViewController.swift
+//  Todo
+//
+//  Created by Kang on 2022/02/27.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
