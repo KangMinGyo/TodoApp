af05292fe3c9b8c090aa9b0c989727e44fc79685
KangMinGyo <kang15567@kakao.com>
2022-03-03 10:57:46 +0900

    feat: Calendar 추가

---
+++ Todo/Controller/CalendarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarViewController.swift
+//  Todo
+//
+//  Created by Kang on 2022/03/03.
+//
+
+import UIKit
+import FSCalendar
+
+class CalendarViewController: UIViewController{
+    
+    @IBOutlet weak var calender: FSCalendar!
+    @IBOutlet weak var todayView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+        todayView.layer.cornerRadius = 15.0
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
